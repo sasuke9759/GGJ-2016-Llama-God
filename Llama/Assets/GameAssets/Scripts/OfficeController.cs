1b1ed868c9592d963ee393608ca299e9047fe655
Alex Jensen <ajensen3@uccs.edu>
2016-01-30 10:37:24 -0700

    Added main office scene

---
+++ Llama/Assets/GameAssets/Scripts/OfficeController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class OfficeController : MonoBehaviour {
+
+    [HideInInspector]
+    public List<GameObject> hallways;
+
+    float[] rotationAmounts = { 0, 90, 180, 270 };
+
+    // Use this for initialization
+    void OnTriggerStay2D(Collider2D collision)
+    {
+	    if (collision.gameObject.tag == "hallway" || collision.gameObject.tag == "room")
+        {
+            //if (GetComponent<Collider2D>().bounds.Contains(collision.gameObject.GetComponent<Collider2D>().bounds.center))
+                PlaceRandomly();
+        }
+	}
+
+    public void PlaceRandomly()
+    {
+        GameObject hallway = hallways[Random.Range(0, hallways.Count)];
+
+        int randomVal = Random.Range(0, 4);
+        Vector3 direction = OfficeGenerator.directions[randomVal];
+        float rotation = rotationAmounts[randomVal];
+        transform.parent.position = hallway.transform.position + direction;
+        transform.parent.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+        transform.parent.Rotate(new Vector3(0, 0, 90));
+        transform.parent.parent = hallway.transform;
+    }
+}
---
+++ Llama/Assets/GameAssets/Scripts/OfficeGenerator.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OfficeGenerator : MonoBehaviour {
+    [SerializeField]
+    int level, numberOfHallways, numberOfOffices, minHallwayLength, maxHallwayLength;
+    [SerializeField]
+    GameObject hallwayPrefab, officePrefab;
+    [SerializeField]
+    GameObject elevator;
+    [SerializeField]
+    GameObject[] requiredRooms;
+
+    List<GameObject> hallwayEndNodes;
+    List<GameObject> hallways, offices;
+
+
+    // right up left down
+    public static Vector3[] directions = { new Vector3(.2f, 0, 0), new Vector3(0, 0,.2f), new Vector3(-.2f,0, 0), new Vector3(0,0, -.2f)};
+    int previousDirection = -1;
+
+    Random random = new Random();
+
+	// Use this for initialization
+	void Start () {
+        hallwayEndNodes = new List<GameObject>();
+        hallways = new List<GameObject>();
+        GenerateFloor();
+	}
+
+    void GenerateFloor()
+    {
+        int hallwaysLeft = numberOfHallways;
+        int officesLeft = numberOfOffices;
+        hallwayEndNodes.Add(elevator);
+
+        while (hallwaysLeft > 0)
+        {
+            GenerateHallways(hallwayEndNodes[Random.Range(0, hallwayEndNodes.Count)]);
+            hallwaysLeft--;
+        }
+        
+        while (officesLeft > 0)
+        {
+            GenerateOffices();
+            officesLeft--;
+        }
+    }
+
+    private void GenerateHallways(GameObject startHallway)
+    {
+        hallwayEndNodes.Remove(startHallway);
+
+        int hallwayLength = Random.Range(minHallwayLength, maxHallwayLength);
+        List<int> directionChoices = new List<int>();
+        for (int i = 0; i < 4; i++)
+        {
+            if ((i+2) % 4 != previousDirection)
+            {
+                directionChoices.Add(i);
+            }
+        }
+
+        int totalChoices = directionChoices.Count;
+        for (int i = 0; i < Random.Range(1, totalChoices); i++)
+        {
+            GameObject currentHallway = startHallway;
+            int direction = Random.Range(0, directionChoices.Count);
+            for (int j = 0; j < hallwayLength; j++)
+            {
+                currentHallway = (GameObject)Instantiate(hallwayPrefab, currentHallway.transform.position + directions[direction], Quaternion.Euler(new Vector3(90, 0, 0)));
+                hallways.Add(currentHallway);
+            }
+            hallwayEndNodes.Add(currentHallway);
+        }        
+    }
+
+    private void GenerateOffices()
+    {
+        OfficeController office = Instantiate(officePrefab).GetComponentInChildren<OfficeController>();
+        office.hallways = hallways;
+        office.PlaceRandomly();
+    }
+}
