c1a6b6811d4c5029605e4f3497eb10e447380eec
JupiterMaroon <arangua.tomyrock.tom@gmail.com>
2016-01-30 12:29:45 -0700

    no change

---
+++ Llama/Assets/GUI/GUI Button Script.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
